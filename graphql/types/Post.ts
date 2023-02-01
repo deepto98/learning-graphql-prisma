1fc6c68c1ed8be50086d722f26ac72a4730bd086
deepto <ddepp98@outlook.com>
2023-02-02 03:31:43 +0530

    added Nexus graphql types for post and queries on Posts and Users

---
+++ graphql/types/Post.ts
@@ -0,0 +1,75 @@
+import { DateTimeResolver } from "graphql-scalars";
+import { asNexusMethod, nonNull, objectType, stringArg } from "nexus";
+
+export const GQLDate = asNexusMethod(DateTimeResolver, 'date');
+
+//Using Nexus for schema construction
+
+export const Post = objectType({
+    name: 'Post',
+    definition(t) {
+        t.int('id');
+        t.date('createdAt');
+        t.string('title');
+        t.nullable.string('content');
+        t.boolean('published');
+        t.nullable.field('author', {
+            type: 'User',
+            async resolve(_parent, _args, ctx) {
+                return await ctx.prisma.post
+                    .findUnique({
+                        where: { id: _parent.id }
+                    })
+                    .author();
+            }
+        })
+
+    }
+});
+
+export const Query = objectType({
+    name: 'Query',
+    definition(t) {
+
+        t.field('post', {
+            type: 'Post',
+            args: {
+                postId: nonNull(stringArg())
+            },
+            async resolve(_, args, ctx) {
+                return await ctx.prisma.post
+                    .findUnique({
+                        where: { id: Number(args.postId) }
+                    })
+                    .author();
+            }
+        });
+
+        t.list.field('feed', {
+            type: 'Post',
+            async resolve(_parent, _args, ctx) {
+                return await ctx.prisma.post.findMany({
+                    where: { published: true }
+                })
+            }
+        });
+
+        t.list.field('drafts', {
+            type: 'Post',
+            async resolve(_parent, _args, ctx) {
+                return await ctx.prisma.post.findMany({
+                    where: { published: false }
+                })
+            }
+        });
+
+        t.list.field('users', {
+            type: 'User',
+            async resolve(_parent, _args, ctx) {
+                return await ctx.prisma.post.findMany({
+                    where: { published: true }
+                })
+            }
+        });
+    }
+})
