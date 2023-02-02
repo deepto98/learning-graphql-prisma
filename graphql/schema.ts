3f488404eef3b8eb048ce291da898b829d2aac08
deepto <ddepp98@outlook.com>
2023-02-02 13:29:07 +0530

    Added schema

---
+++ graphql/schema.ts
@@ -0,0 +1,12 @@
+import { connectionPlugin, makeSchema } from "nexus";
+import * as types from './types';
+import { join } from "path";
+
+export const schema = makeSchema({
+    plugins: [connectionPlugin()],
+    types,
+    outputs: {
+        typegen: join(process.cwd(), 'generated/nexus-typegen.ts'),
+        schema: join(process.cwd(), 'generated/schema.graphql')
+    }
+});
