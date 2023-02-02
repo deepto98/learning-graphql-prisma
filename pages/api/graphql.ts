63436e3deadfb8f123caae21ca862e0cbfe45abe
deepto <ddepp98@outlook.com>
2023-02-02 13:33:42 +0530

    Added schema and context to apollo server, updated .env, added auto generated files, current status: graphql server runs on endpoint

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,8 +1,10 @@
 import { ApolloServer } from 'apollo-server-micro';
 import { NextApiRequest, NextApiResponse, PageConfig } from 'next';
+import { schema } from '../../graphql/schema';
+import { createContext } from '../../graphql/context';
 
 // Apollo GraphQL Server
-const apolloServer = new ApolloServer({});
+const apolloServer = new ApolloServer({ schema, context: createContext });
 const startServer = apolloServer.start();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
