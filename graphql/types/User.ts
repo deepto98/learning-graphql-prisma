172d862d76166e74e3fa871b2eaf705765ee5f6d
deepto <ddepp98@outlook.com>
2023-02-02 13:24:00 +0530

    Added User type

---
+++ graphql/types/User.ts
@@ -0,0 +1,20 @@
+import { objectType } from "nexus";
+
+export const User = objectType({
+    name: 'User',
+    definition(t) {
+        t.int('id');
+        t.string('title');
+        t.string('email');
+        t.list.field('posts', {
+            type: 'Post',
+            async resolve(_parent, _args, ctx) {
+                return await ctx.prisma.user
+                    .findUnique({
+                        where: { id: _parent.id }
+                    })
+                    .posts();
+            }
+        });
+    }
+});
