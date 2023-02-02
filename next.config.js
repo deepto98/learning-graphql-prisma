65f94a28e9d2609e88a26c8cb06207a547e7639f
deepto <ddepp98@outlook.com>
2023-02-02 13:44:40 +0530

    Fixes, current status: graphql server works, CORS issue is solved

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: 'api/graphql',
+        source: '/api/graphql',
         headers: [
-          { key: 'Access-Control-Allow-Credentials', value: true },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: 'https://studio.apollographql.com' },
           { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
         ]
